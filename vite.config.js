5754d52413644c1ed02b9ffeae3be1800303ab8a
RABE Marcellin <marcellinr.rabe@gmail.com>
2024-12-06 21:09:45 +0300

    Config ffmpeg for production

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import crossOriginIsolation from "vite-plugin-cross-origin-isolation";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  headers: {
+    'Cross-Origin-Embedder-Policy': 'require-corp',
+    'Cross-Origin-Opener-Policy': 'same-origin',
+  },
   plugins: [react(), crossOriginIsolation()],
   optimizeDeps: {
     exclude: ["@ffmpeg/ffmpeg"], // Add ffmpeg.js or the related worker here
