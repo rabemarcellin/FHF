c9f971688dbba4bb88070ea0887cebf5a274c720
RABE Marcellin <marcellinr.rabe@gmail.com>
2024-12-07 09:35:21 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,12 +8,9 @@ export default defineConfig({
     exclude: ["@ffmpeg/ffmpeg"],
   },
   server: {
-    configureServer: (server) => {
-      server.middlewares.use((req, res, next) => {
-        res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
-        res.setHeader("Cross-Origin-Embedder-Policy", "credentialless");
-        next();
-      });
+    headers: {
+      "Cross-Origin-Opener-Policy", "same-origin",
+      "Cross-Origin-Embedder-Policy", "require-corp"
     },
   },
   plugins: [react(), crossOriginIsolation()],
