ee808457345444a0ed374f08571b8194c89e4cbc
RABE Marcellin <marcellinr.rabe@gmail.com>
2024-12-07 17:36:50 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     headers: {
       "Cross-Origin-Opener-Policy": "same-origin",
-      "Cross-Origin-Embedder-Policy": "require-corp",
+      "Cross-Origin-Embedder-Policy": "credentialless",
       "Cross-Origin-Resource-Policy": "cross-origin"
     },
   },
