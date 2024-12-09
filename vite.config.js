e532ea03cf0fc2797bfecd6920b57297118acfc9
rabemarcellin <marcellinr.rabe@gmail.com>
2024-12-09 10:41:34 +0300

    change cross-isolation config

--- vite.config.js
+++ vite.config.js
@@ -15,5 +15,18 @@ export default defineConfig({
        */
     },
   },
-  plugins: [react(), crossOriginIsolation()],
+  plugins: [
+    react(),
+    /* crossOriginIsolation(), */
+    {
+      name: "configure-response-headers",
+      configureServer: (server) => {
+        server.middlewares.use((_req, res, next) => {
+          res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+          res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+          next();
+        });
+      },
+    },
+  ],
 });
